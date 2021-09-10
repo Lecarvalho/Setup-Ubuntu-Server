84abb122aa3eee46824278a37fa2c599909c7294
Leandro Carvalho <5149784+Lecarvalho@users.noreply.github.com>
2021-09-10 13:55:21 -0400

    Allowing ports 80 and 443 (#2)
    * allow ports
    
    * checks before create and install stuffs
    
    * fix script
    
    * fix script

--- scripts/create-workspace-dir.sh
+++ scripts/create-workspace-dir.sh
@@ -1,5 +1,1 @@
-if [ -d "/Workspace" ]; then
-    echo "Directory already exists";
-else
-    `sudo mkdir -m 777 /Workspace`;
-fi
+sudo mkdir -m 777 /Workspace
--- scripts/setup-env.sh
+++ scripts/setup-env.sh
@@ -1,3 +1,44 @@
-sh create-workspace-dir.sh
-sh install-docker.sh
-sh install-docker-compose.sh
+service_exists() {
+    local n=$1
+    if service --status-all | grep -Fq $n; then
+        return 0
+    else
+        return 1
+    fi
+}
+
+file_exists() {
+    if [ -f $1 ]; then
+        return 0
+    else 
+        return 1
+    fi
+}
+
+directory_exists() {
+    if [ -d $1 ]; then
+        return 0
+    else
+        return 1
+    fi
+}
+
+if directory_exists "/Workspace"; then
+    echo "/Workspace already exists";
+else 
+    sh create-workspace-dir.sh
+fi
+
+if service_exists docker; then
+    echo "docker already installed"
+else 
+    sh install-docker.sh
+fi
+
+if file_exists "/usr/local/bin/docker-compose"; then
+    echo "docker-compose already installed"
+else
+    sh install-docker-compose.sh
+fi
+
+sh allow-ports.sh
